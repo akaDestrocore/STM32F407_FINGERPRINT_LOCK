87196aeb2665137bb40cb15c7e260ec9bada9bfd
MYKOLA ABLAPOKHIN <93255514+akaDestrocore@users.noreply.github.com>
2024-02-19 14:20:21 +0300

    Delete Debug directory

--- Debug/bsp/st7735_fonts.d
+++
@@ -1,3 +0,0 @@
-bsp/st7735_fonts.o: ../bsp/st7735_fonts.c \
- D:/Users/nabla/STM32CubeIDE/TI_like_workspace/FingerprintLock/bsp/st7735_fonts.h
-D:/Users/nabla/STM32CubeIDE/TI_like_workspace/FingerprintLock/bsp/st7735_fonts.h:
